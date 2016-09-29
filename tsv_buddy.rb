5a0a7159b529dc5a1e620e123dfbdcd60fd48928
Cheng-Yu Hsu <m@cyhsu.me>
2016-09-29 10:53:22 +0800

    TSV buddy

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,41 @@
+# frozen_string_literal: true
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
+  require 'csv'
+
+  TAB = "\t"
+
+  TSV_OPTIONS = { col_sep: TAB }.freeze
+
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    tsv_data = CSV.parse(tsv, TSV_OPTIONS)
+    column_names = tsv_data.shift
+    @data = tsv_data.map { |row| hashify(column_names, row) }
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
+    keys = extract_keys(@data)
+    CSV.generate(TSV_OPTIONS) do |csv|
+      csv << keys
+      @data.each { |record| csv << to_sorted_values(record, using_keys: keys) }
+    end
+  end
 
+  private
+
+  def hashify(keys, values)
+    keys.zip(values).to_h
+  end
+
+  def extract_keys(data)
+    data.map(&:keys).flatten.uniq
+  end
+
+  def to_sorted_values(record, using_keys: keys)
+    using_keys.map { |key| record[key] }
   end
 end
