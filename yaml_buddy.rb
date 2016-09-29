c821f6ef8cb11b7524aa041206dc42108632e691
Cheng-Yu Hsu <m@cyhsu.me>
2016-09-29 10:53:34 +0800

    YML buddy

--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,16 @@
+# frozen_string_literal: true
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  require 'yaml'
+  # take_tsv: converts a String with TSV data into @data
+  # parameter: tsv - a String in TSV format
+  def take_yaml(yaml)
+    @data = YAML.load(yaml)
+  end
+
+  # to_tsv: converts @data into tsv string
+  # returns: String in TSV format
+  def to_yaml
+    @data.to_yaml
+  end
+end
